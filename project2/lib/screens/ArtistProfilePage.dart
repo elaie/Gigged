b1fadbfaaf8c1e94929614d5de2ec5bbfdf30f96
Sanskar Shrestha <elaiextha@gmail.com>
2022-10-09 22:01:30 +0545

    minor fixes

--- project2/lib/screens/ArtistProfilePage.dart
+++ project2/lib/screens/ArtistProfilePage.dart
@@ -35,69 +35,16 @@ class _ArtistProfilePageState extends State<ArtistProfilePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-            Text(
-            "Hello there!",
-            style: TextStyle(
-                fontFamily: 'Comfortaa',
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: kPrimaryColor),
-          ),
-          Text(
-              extractData().getUserName(),
-            style: TextStyle(
-                fontFamily: 'Comfortaa',
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: kPrimaryColor),
-          ),
-
-          //profile picture
-          GestureDetector(
-            onTap: () {
-              print('image pressed');
-            },
-
-
-
-              child : FutureBuilder(future: storage.downloadURL(ExtractData.getUserUID()),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<String> snapshot) {
-
-                    print("===================FUTURE BUILDER LIST FILE INITIALIZED=======================");
-                    extractData().getUserUID();
-                    if (snapshot.connectionState == ConnectionState.done &&
-                        snapshot.hasData) {
-                      print("CONNECTION STATE IS STABLE");
-                      return Container(width: 300,
-                          height: 250,
-                          child: Image.network(snapshot.data!,
-                              fit:BoxFit.cover));
-                    }
-                    print("CONNECTION STATE IS UN-STABLE");
-                    return Container();
-                  })
-          ),
-          //edit button
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-          ElevatedButton(
-          child: Text(
-          "Edit Profile",
-            style: TextStyle(
-                fontFamily: 'Comfortaa', fontWeight: FontWeight.bold),
-          ),
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(
-              (kPrimaryColor),
-            ),
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(25.0),
+              Text(
+                "Hello there!",
+                style: TextStyle(
+                    fontFamily: 'Comfortaa',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: kPrimaryColor),
               ),
               Text(
-                extractData().getUserEmail(),
+                extractData().getUserName(),
                 style: TextStyle(
                     fontFamily: 'Comfortaa',
                     fontWeight: FontWeight.bold,
@@ -186,14 +133,14 @@ class _ArtistProfilePageState extends State<ArtistProfilePage> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: List.generate(
                       5,
-                      (index) => IconButton(
+                          (index) => IconButton(
                         icon: index < _rating
                             ? Icon(Icons.star, size: 32)
                             : Icon(Icons.star_border, size: 32),
                         color: kPrimaryColor,
                         onPressed: () {
                           setState(
-                            () {
+                                () {
                               _rating = index + 1;
                             },
                           );
@@ -257,7 +204,7 @@ class _ArtistProfilePageState extends State<ArtistProfilePage> {
 Route _createRoute() {
   return PageRouteBuilder(
     pageBuilder: (context, animation, secondaryAnimation) =>
-        const EditProfilePage(),
+    const EditProfilePage(),
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
       const begin = Offset(0.0, 1.0);
       const end = Offset.zero;
