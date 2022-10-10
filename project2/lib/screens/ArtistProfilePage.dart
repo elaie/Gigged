4ff1bca9bf9b56213b4bde55dcc4e218c424aa91
Sanskar Shrestha <elaiextha@gmail.com>
2022-10-10 16:02:27 +0545

    added BIO

--- project2/lib/screens/ArtistProfilePage.dart
+++ project2/lib/screens/ArtistProfilePage.dart
@@ -1,23 +1,53 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:project2/screens/EditProfilePage.dart';
 import 'package:project2/screens/constraints.dart';
 import 'package:project2/screens/welcome_screen.dart';
 import '../storage_services.dart';
 import '../getData.dart';
+import '../local_data.dart';
 
 class ArtistProfilePage extends StatefulWidget {
   const ArtistProfilePage({Key? key}) : super(key: key);
 
   @override
   State<ArtistProfilePage> createState() => _ArtistProfilePageState();
+
+
 }
 
 class _ArtistProfilePageState extends State<ArtistProfilePage> {
+  final _database = FirebaseDatabase.instance.reference();
+
+  String displayBio = " ";
+  late StreamSubscription _userBio;
+
   final Storage storage = Storage();
   final extractData ExtractData = extractData();
-
+  final local_data Local_data = local_data();
   var _rating = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    activateListeners();
+  }
+
+  void activateListeners() {
+    _userBio = _database
+        .child(ExtractData.getUserUID() + "/BIO/Bio")
+        .onValue
+        .listen((event) {
+      final String description = event.snapshot.value.toString();
+      print("LISTENER: " + description);
+      setState(() {
+        displayBio = '$description';
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,6 +73,7 @@ class _ArtistProfilePageState extends State<ArtistProfilePage> {
                     fontSize: 20,
                     color: kPrimaryColor),
               ),
+
               Text(
                 extractData().getUserName(),
                 style: TextStyle(
@@ -61,19 +92,19 @@ class _ArtistProfilePageState extends State<ArtistProfilePage> {
                       future: storage.downloadURL(ExtractData.getUserUID()),
                       builder: (BuildContext context,
                           AsyncSnapshot<String> snapshot) {
-                        print(
-                            "===================FUTURE BUILDER LIST FILE INITIALIZED=======================");
+                        // print(
+                        // "===================FUTURE BUILDER LIST FILE INITIALIZED=======================");
                         extractData().getUserUID();
                         if (snapshot.connectionState == ConnectionState.done &&
                             snapshot.hasData) {
-                          print("CONNECTION STATE IS STABLE");
+                          //print("CONNECTION STATE IS STABLE");
                           return CircleAvatar(
                               radius: 70,
                               backgroundImage: NetworkImage(
                                 snapshot.data!,
                               ));
                         }
-                        print("CONNECTION STATE IS UN-STABLE");
+                        // print("CONNECTION STATE IS UN-STABLE");
                         return Container();
                       })),
               //edit button
@@ -109,13 +140,6 @@ class _ArtistProfilePageState extends State<ArtistProfilePage> {
                       );
                     },
                   ),
-
-                  /*onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const EditProfilePage()),
-                    );
-                  },*/
                 ],
               ),
               //ratings
@@ -133,19 +157,20 @@ class _ArtistProfilePageState extends State<ArtistProfilePage> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: List.generate(
                       5,
-                          (index) => IconButton(
-                        icon: index < _rating
-                            ? Icon(Icons.star, size: 32)
-                            : Icon(Icons.star_border, size: 32),
-                        color: kPrimaryColor,
-                        onPressed: () {
-                          setState(
-                                () {
-                              _rating = index + 1;
+                          (index) =>
+                          IconButton(
+                            icon: index < _rating
+                                ? Icon(Icons.star, size: 32)
+                                : Icon(Icons.star_border, size: 32),
+                            color: kPrimaryColor,
+                            onPressed: () {
+                              setState(
+                                    () {
+                                  _rating = index + 1;
+                                },
+                              );
                             },
-                          );
-                        },
-                      ),
+                          ),
                     )),
                 duration: Duration(milliseconds: 300),
               ),
@@ -158,16 +183,17 @@ class _ArtistProfilePageState extends State<ArtistProfilePage> {
                     fontSize: 16,
                     color: kPrimaryDarkColor),
               ),
+
               Padding(
                 padding: const EdgeInsets.all(20),
-                child: Text(
-                  "*insert bio here*",
+                child: Text(displayBio,
                   style: TextStyle(
                       fontFamily: 'Comfortaa',
                       fontWeight: FontWeight.bold,
                       fontSize: 16,
                       color: kPrimaryDarkColor),
                 ),
+
               ),
 
               //logout button
@@ -198,8 +224,16 @@ class _ArtistProfilePageState extends State<ArtistProfilePage> {
         ),
       ),
     );
+
   }
+  @override
+  void deactivate () {
+    _userBio.cancel();
+    super.deactivate();
+  }
+
 }
+
 
 Route _createRoute() {
   return PageRouteBuilder(
@@ -219,3 +253,4 @@ Route _createRoute() {
     },
   );
 }
+
