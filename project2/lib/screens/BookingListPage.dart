1704f70b7e0754c730ca5e7e0ddbd1f1ddc8b690
elaie <elaiextha@gmail.com>
2022-11-04 17:18:56 +0545

    Booking backend

--- project2/lib/screens/BookingListPage.dart
+++ project2/lib/screens/BookingListPage.dart
@@ -1,12 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-
+import 'package:project2/screens/PublicArtistProfile.dart';
+import '../storage_services.dart';
+import 'package:project2/getData.dart';
 class BookingListPage extends StatelessWidget {
   const BookingListPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    final Storage storage = Storage();
+    final extractData ExtractData = extractData();
+    final uid = extractData().getUserUID().toString();
+    return Scaffold(
+      body: StreamBuilder<QuerySnapshot>(
 
+        stream: FirebaseFirestore.instance.collection('Venue').doc(uid).collection('Booking List').snapshots(),
+        builder: (context, snapshots) {
+          return (snapshots.connectionState == ConnectionState.waiting)
+              ? Center(
+            child: CircularProgressIndicator(),
+          )
+              : ListView.separated(
+              separatorBuilder: (context, index) {
+                return Divider();
+              },
+              itemCount: snapshots.data!.docs.length,
+              itemBuilder: (context, index) {
+                var data = snapshots.data!.docs[index].data()
+                as Map<String, dynamic>;
+                return GestureDetector(
+                    onTap: () {
+                      print("Tapped ");
+                      //POP UP
+                      showDialog(context: context, builder: (context)=> AlertDialog(
+                        title: Text("Name: "+data['Name']),
+                        //title: Text("Contact: "+data['Contact']),
+                        //title: Text("Email Address: "+data['Email']),
+                        //title: Text("No Of Attendees: "+data['No Of Attendees']),
+                      ));
+                      print("nav pushed");
+                    },
+
+                    //radius vairacha somehow
+                    child: Card(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Text(data['Name'].toString()),
+                        ],
+                      ),
+                    ));
+              });
+        },
+      ),
     );
   }
 }
--- project2/lib/screens/BookingOrReg.dart
+++ project2/lib/screens/BookingOrReg.dart
@@ -1,15 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 import 'constraints.dart';
 
 class BookingOrReg extends StatefulWidget {
-  const BookingOrReg({Key? key}) : super(key: key);
-
+  //const BookingOrReg({Key? key}) : super(key: key);
+final venueUID;
+BookingOrReg(this.venueUID);
   @override
   State<BookingOrReg> createState() => _BookingOrRegState();
 }
 
 class _BookingOrRegState extends State<BookingOrReg> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _noOfAtController = TextEditingController();
+  TextEditingController _contactController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,6 +91,7 @@ class _BookingOrRegState extends State<BookingOrReg> {
                 Padding(
                   padding: const EdgeInsets.only(left: 10.0, right: 10),
                   child: TextFormField(
+                    controller: _emailController,
                     decoration: InputDecoration(
                       focusedBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(30.0),),
@@ -109,6 +117,7 @@ class _BookingOrRegState extends State<BookingOrReg> {
                 Padding(
                   padding: const EdgeInsets.only(left: 10.0, right: 10),
                   child: TextFormField(
+                    controller: _nameController,
                     decoration: InputDecoration(
                       focusedBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(30.0),),
@@ -133,6 +142,7 @@ class _BookingOrRegState extends State<BookingOrReg> {
                 Padding(
                   padding: const EdgeInsets.only(left: 10.0, right: 10),
                   child: TextFormField(
+                    controller: _noOfAtController,
                     decoration: InputDecoration(
                       focusedBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(30.0),),
@@ -157,6 +167,7 @@ class _BookingOrRegState extends State<BookingOrReg> {
                 Padding(
                   padding: const EdgeInsets.only(left: 10.0, right: 10),
                   child: TextFormField(
+                    controller: _contactController,
                     decoration: InputDecoration(
                       focusedBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.all(Radius.circular(30.0),),
@@ -187,8 +198,24 @@ class _BookingOrRegState extends State<BookingOrReg> {
                 SizedBox(
                   height: 50, //height of button
                   width: 200,
+
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Map<String, String> dataToSave = {
+                        'Email': _emailController.text,
+                        'Name': _nameController.text,
+                        'No Of Attendees': _noOfAtController.text,
+                        'Contact':_contactController.text,
+                      };
+                      FirebaseFirestore.instance
+                          .collection("Venue").doc(widget.venueUID).collection("Booking List")
+                          .add(dataToSave).then((value) {
+                            print("EMAIL:ASJLHAGL"+_emailController.text);
+                        Fluttertoast.showToast(msg: "Successfully Registered!");
+                        Navigator.pop(context);
+                      });
+
+                    },
                     child: const Text(
                       'Register',
                       style: TextStyle(
